79464108e2ec95f58674b9a6a1c9ba6f205f8ee5
Saúl Gutiérrez <62368834+saulgutierrez@users.noreply.github.com>
2024-05-16 14:14:05 -0600

    Mostrar mensaje de error en caso de que se duplique el nombre de usuario en el form de registro.

--- php/new-user.php
+++ php/new-user.php
@@ -7,17 +7,29 @@
         $user = $_POST['user'];
         $pass = $_POST['password'];
 
-        $cryptPass = sha1($pass);
-        # Insertamos en la base de datos
-        $sql = "INSERT INTO usuarios (id, usuario, contrasenia, nombre, apellido, correo, telefono, fecha_nacimiento, genero, fotografia) VALUES ('', '$user', '$cryptPass', '', '', '', '', '', '', '')";
-        
-        if ($conn->query($sql) == TRUE) {
-            $_SESSION['user'] = $user;
-            header('Location: dashboard.php');
+        $cryptPass = sha1($pass); # Encriptamos la contrasenia
+
+        # Realizamos un chequeo para evitar nombres de usuario duplicados
+        $sql_check = "SELECT COUNT(*) AS count FROM usuarios WHERE usuario = '$user'";
+        $statement_check = $conn->prepare($sql_check);
+        $statement_check->execute();
+        $result_check = $statement_check->get_result();
+        $row = $result_check->fetch_assoc();
+
+        # Si el nombre de usuario es unico, insertamos en la base de datos
+        if ($row['count'] == 0) {
+            $sql = "INSERT INTO usuarios (id, usuario, contrasenia, nombre, apellido, correo, telefono, fecha_nacimiento, genero, fotografia) VALUES ('', '$user', '$cryptPass', '', '', '', '', '', '', '')";
+            
+            if ($conn->query($sql) == TRUE) {
+                $_SESSION['user'] = $user;
+                header('Location: dashboard.php');
+            } else {
+                echo "Error al insertar registro";
+            }
+            $conn->close();
         } else {
-            echo "Error al insertar registro";
+            echo "Por favor, seleccione otro nombre de usuario";
         }
-        $conn->close();
     } else {
         echo "Error de comunicacion con el servidor: ".$conn->error;
         $conn->close();
