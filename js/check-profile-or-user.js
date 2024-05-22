3fb3f4dc01ca4f28874728e5cc802bbe9bdeca33
Saúl Gutiérrez <62368834+saulgutierrez@users.noreply.github.com>
2024-05-22 15:24:09 -0600

    Ocultamos el menu de gestion de posteos cuando un usuario loguedo, visualice el perfil de otro usuario, no logueado.

--- js/check-profile-or-user.js
+++ js/check-profile-or-user.js
@@ -2,6 +2,7 @@ const params = new URLSearchParams(window.location.search);
 
 const btn1 = document.getElementById('btn-1');
 const btn2 = document.getElementById('btn-2');
+const menuIconos = document.querySelectorAll('.menu-icon');
 
 if (params.has('user')) {
     if (btn1) {
@@ -11,11 +12,23 @@ if (params.has('user')) {
     if (btn2) {
         btn2.classList.remove('hidden');
     }
+
+    if (menuIconos) {
+        menuIconos.forEach(elemento => {
+            elemento.classList.remove('hidden');
+        });
+    }
 } else if (params.has('id')) {
     if (btn1) {
         btn1.classList.add('hidden');
     }
     if (btn2) {
         btn2.classList.add('hidden');
+    }
+
+    if (menuIconos) {
+        menuIconos.forEach(elemento => {
+            elemento.classList.add('hidden');
+        });
     }
 }
