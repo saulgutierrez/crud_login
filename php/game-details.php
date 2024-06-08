351e1cb2503ca53503404aaaaa3bb822a52d1112
Saúl Gutiérrez <62368834+saulgutierrez@users.noreply.github.com>
2024-06-07 21:16:09 -0600

    Estilizar la página de detalles de cada juego, consultando datos desde la API. Aún no finalizado.

--- php/game-details.php
+++ php/game-details.php
@@ -28,53 +28,63 @@
     <link rel="stylesheet" href="../css/game-details.css">
     <title><?php echo htmlspecialchars($gameData['title']); ?></title>
 </head>
+<?php include "../includes/navbar-game-details.php"; ?>
 <body>
-    <?php include "../includes/navbar.php"; ?>
     <!-- Reestructuracion del codigo que consume la API, con comprobaciones, para evitar warnings en caso
         de que existan campos vacions con informacion no disponible. -->
     <div class="main-container">
-        <h1 class="titulo"><?php echo isset($gameData['title']) ? htmlspecialchars($gameData['title']) : 'Título no disponible'; ?></h1>
 
         <?php if (isset($gameData['thumbnail']) && !empty($gameData['thumbnail'])) { ?>
             <div class="header">
-                <div class="main-image-container">
-                    <img src="<?php echo htmlspecialchars($gameData['thumbnail']); ?>" alt="Imagen del juego">
+                <h1 class="titulo"><?php echo isset($gameData['title']) ? htmlspecialchars($gameData['title']) : 'Título no disponible'; ?></h1>
+                <div class="top-container">
+                    <div class="main-image-container">
+                        <img src="<?php echo htmlspecialchars($gameData['thumbnail']); ?>" alt="Imagen del juego">
+                    </div>
+                    <p class="game-description"><?php echo isset($gameData['description']) ? htmlspecialchars($gameData['description']) : 'Descripción no disponible'; ?></p>
                 </div>
-                <p><?php echo isset($gameData['description']) ? htmlspecialchars($gameData['description']) : 'Descripción no disponible'; ?></p>
             </div>
         <?php } else { ?>
             <p>Imagen del juego no disponible</p>
         <?php } ?>
-        <div class="genre">
-            <p>Genre</p>
-            <p><?php echo isset($gameData['genre']) ? htmlspecialchars($gameData['genre']) : 'Género no disponible'; ?></p>
+        <div class="wrapper-features">
+            <div class="genre">
+                <p>Genre</p>
+                <p><?php echo isset($gameData['genre']) ? htmlspecialchars($gameData['genre']) : 'Género no disponible'; ?></p>
+            </div>
+            <div class="platform">
+                <p>Platform</p>
+                <p><?php echo isset($gameData['platform']) ? htmlspecialchars($gameData['platform']) : 'Plataforma no disponible'; ?></p>
+            </div>
+            <div class="publisher">
+                <p>Publisher</p>
+                <p><?php echo isset($gameData['publisher']) ? htmlspecialchars($gameData['publisher']) : 'Publicador no disponible'; ?></p>
+            </div>
         </div>
-        <div class="platform">
-            <p>Platform</p>
-            <p><?php echo isset($gameData['platform']) ? htmlspecialchars($gameData['platform']) : 'Plataforma no disponible'; ?></p>
-        </div>
-        <div class="publisher">
-            <p>Publisher</p>
-            <p><?php echo isset($gameData['publisher']) ? htmlspecialchars($gameData['publisher']) : 'Publicador no disponible'; ?></p>
-        </div>
-        <div class="developer">
-            <p>Developer</p>
-            <p><?php echo isset($gameData['developer']) ? htmlspecialchars($gameData['developer']) : 'Desarrollador no disponible'; ?></p>
-        </div>
-        <div class="release-date">
-            <p>Release Date</p>
-            <p><?php echo isset($gameData['release_date']) ? htmlspecialchars($gameData['release_date']) : 'Fecha de lanzamiento no disponible'; ?></p>
-        </div>
+        <div class="wrapper-features">
+            <div class="developer">
+                <p>Developer</p>
+                <p><?php echo isset($gameData['developer']) ? htmlspecialchars($gameData['developer']) : 'Desarrollador no disponible'; ?></p>
+            </div>
+            <div class="release-date">
+                <p>Release Date</p>
+                <p><?php echo isset($gameData['release_date']) ? htmlspecialchars($gameData['release_date']) : 'Fecha de lanzamiento no disponible'; ?></p>
+            </div>
 
-        <?php if (isset($gameData['minimum_system_requirements'])) { ?>
-            <p><?php echo isset($gameData['minimum_system_requirements']['os']) ? htmlspecialchars($gameData['minimum_system_requirements']['os']) : 'Requisitos mínimos del sistema operativo no disponibles'; ?></p>
-            <p><?php echo isset($gameData['minimum_system_requirements']['processor']) ? htmlspecialchars($gameData['minimum_system_requirements']['processor']) : 'Requisitos mínimos del procesador no disponibles'; ?></p>
-            <p><?php echo isset($gameData['minimum_system_requirements']['memory']) ? htmlspecialchars($gameData['minimum_system_requirements']['memory']) : 'Requisitos mínimos de memoria no disponibles'; ?></p>
-            <p><?php echo isset($gameData['minimum_system_requirements']['graphics']) ? htmlspecialchars($gameData['minimum_system_requirements']['graphics']) : 'Requisitos mínimos de gráficos no disponibles'; ?></p>
-            <p><?php echo isset($gameData['minimum_system_requirements']['storage']) ? htmlspecialchars($gameData['minimum_system_requirements']['storage']) : 'Requisitos mínimos de almacenamiento no disponibles'; ?></p>
-        <?php } else { ?>
-            <p>Requisitos mínimos del sistema no disponibles</p>
-        <?php } ?>
+            <?php if (isset($gameData['minimum_system_requirements'])) { ?>
+                <div class="requirements">
+                    <p><?php echo isset($gameData['minimum_system_requirements']['os']) ? htmlspecialchars($gameData['minimum_system_requirements']['os']) : 'Requisitos mínimos del sistema operativo no disponibles'; ?></p>
+                    <p><?php echo isset($gameData['minimum_system_requirements']['processor']) ? htmlspecialchars($gameData['minimum_system_requirements']['processor']) : 'Requisitos mínimos del procesador no disponibles'; ?></p>
+                    <p><?php echo isset($gameData['minimum_system_requirements']['memory']) ? htmlspecialchars($gameData['minimum_system_requirements']['memory']) : 'Requisitos mínimos de memoria no disponibles'; ?></p>
+                    <p><?php echo isset($gameData['minimum_system_requirements']['graphics']) ? htmlspecialchars($gameData['minimum_system_requirements']['graphics']) : 'Requisitos mínimos de gráficos no disponibles'; ?></p>
+                    <p><?php echo isset($gameData['minimum_system_requirements']['storage']) ? htmlspecialchars($gameData['minimum_system_requirements']['storage']) : 'Requisitos mínimos de almacenamiento no disponibles'; ?></p>
+                </div>
+            <?php } else { ?>
+                <div class="requirements">
+                    <p>Requisitos mínimos del sistema no disponibles</p>
+                </div>
+            <?php } ?>
+        </div>
 
         <div class="img-container">
             <?php if (!empty($gameData['screenshots']) && is_array($gameData['screenshots'])) {
