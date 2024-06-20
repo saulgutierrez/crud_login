b68aa7b1503d9420c6c50641bc6414c2336944b2
Saúl Gutiérrez <62368834+saulgutierrez@users.noreply.github.com>
2024-06-19 19:25:34 -0600

    Podemos crear posteos con fotografia o sin fotografia desde nuestro perfil.

--- php/post-data.php
+++ php/post-data.php
@@ -20,6 +20,9 @@ if (isset($_POST['id_user'], $_POST['user'], $_POST['post_title'], $_POST['post_
     $postTitle = $_POST['post_title'];
     $postContent = $_POST['post_content'];
 
+    $file_uploaded = false; // Bandera para verificar si el post será con fotografia o no
+    $target_file = null;
+
     if (isset($_FILES['file']) && $_FILES['file']['error'] == UPLOAD_ERR_OK) {
         $filename = basename($_FILES["file"]["name"]);
         $target_file = $target_dir . $filename;
@@ -27,7 +30,7 @@ if (isset($_POST['id_user'], $_POST['user'], $_POST['post_title'], $_POST['post_
         // Verificar tipo MIME
         $allowed_mime_types = ['image/jpeg', 'image/png', 'image/gif'];
         $file_mime_type = mime_content_type($_FILES['file']['tmp_name']);
-        
+
         if (!in_array($file_mime_type, $allowed_mime_types)) {
             $response['message'] = "Tipo de archivo no permitido.";
             echo json_encode($response);
@@ -36,26 +39,34 @@ if (isset($_POST['id_user'], $_POST['user'], $_POST['post_title'], $_POST['post_
 
         // Mover archivo subido a la carpeta de destino
         if (move_uploaded_file($_FILES["file"]["tmp_name"], $target_file)) {
-            // Preparamos la consulta y vinculamos los parámetros correctamente
-            $sql = $conn->prepare("INSERT INTO post (id_post, id_autor, autor_post, titulo_post, contenido_post, foto_post) VALUES (NULL, ?, ?, ?, ?, ?)");
-             // Corregido el primer parámetro de 'bind_param' a 'issss' para representar los tipos de las variables
-            $sql->bind_param('issss', $idPerfil, $username, $postTitle, $postContent, $target_file);
-
-            if ($sql->execute()) {
-                $response['status'] = 'success';
-                $response['message'] = "El archivo se ha subido y los datos se han guardado en la base de datos.";
-            } else {
-                $response['message'] = "Error al ejecutar la consulta: " . $sql->error;
-            }
-            $sql->close(); // Cerramos el statement después de la ejecución
+            $file_uploaded = true; // Si se subio un fichero, cambiamos la bandera a true
         } else {
             $response['message'] = "Error en la subida del archivo.";
             $response['file_error'] = $_FILES['file']['error'];
+            echo json_encode($response);
+            exit;
+        }
+    }
+
+    // Preparamos la consulta y vinculamos los parámetros correctamente
+    if ($file_uploaded) { // Si se subio una fotografia, tomamos su path, y lo almacenamos en la base de datos
+        $sql = $conn->prepare("INSERT INTO post (id_post, id_autor, autor_post, titulo_post, contenido_post, foto_post) VALUES (NULL, ?, ?, ?, ?, ?)");
+        $sql->bind_param('issss', $idPerfil, $username, $postTitle, $postContent, $target_file);
+    } else { // En caso contrario, guardamos una cadena vacía
+        $sql = $conn->prepare("INSERT INTO post (id_post, id_autor, autor_post, titulo_post, contenido_post) VALUES (NULL, ?, ?, ?, ?)");
+        $sql->bind_param('isss', $idPerfil, $username, $postTitle, $postContent);
+    }
+
+    if ($sql->execute()) {
+        $response['status'] = 'success';
+        $response['message'] = "Los datos se han guardado en la base de datos.";
+        if ($file_uploaded) {
+            $response['message'] .= " El archivo se ha subido correctamente.";
         }
     } else {
-        $response['message'] = "Error en la subida del archivo.";
-        $response['file_error'] = $_FILES['file']['error'];
+        $response['message'] = "Error al ejecutar la consulta: " . $sql->error;
     }
+    $sql->close(); // Cerramos el statement después de la ejecución
 } else {
     $response['message'] = "No se recibieron los datos desde el frontend.";
 }
