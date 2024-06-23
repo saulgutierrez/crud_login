75829004228b5fb7c3fb2604134b9206b7ac4929
Saúl Gutiérrez <62368834+saulgutierrez@users.noreply.github.com>
2024-06-23 13:56:09 -0600

    Eliminamos la imagen de perfil del servidor al eliminar el perfil del usuario.

--- php/delete-data.php
+++ php/delete-data.php
@@ -12,16 +12,24 @@
 
         $cryptPass = sha1($pass);
 
-        $sql_check = "SELECT COUNT(*) AS count FROM usuarios WHERE usuario = '$user' AND contrasenia = '$cryptPass'";
+        $sql_check = "SELECT COUNT(*) AS count, fotografia FROM usuarios WHERE usuario = '$user' AND contrasenia = '$cryptPass'";
         $statement_check = $conn->prepare($sql_check);
         $statement_check->execute();
         $result_check = $statement_check->get_result();
         $row = $result_check->fetch_assoc();
         
         if ($row['count'] == 1) {
+            // Obtener la ruta de la imagen de perfil
+            $file_path = $row['fotografia'];
+
             // Eliminar el perfil del usuario
             $sql = "DELETE FROM usuarios WHERE usuario = '$user' AND contrasenia = '$cryptPass'";
             $result = $conn->query($sql);
+
+            // Eliminamos la imagen del usuario del servidor en caso de que exista
+            if (file_exists($file_path)) {
+                unlink($file_path);
+            }
 
             // Eliminar posteos del usuario
             $sql2 = "DELETE FROM post WHERE autor_post = '$user'";
