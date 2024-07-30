ed4140799daf21bca8ec3e2cdaeb98b33562810d
Saúl Gutiérrez <62368834+saulgutierrez@users.noreply.github.com>
2024-07-29 20:00:00 -0600

    Agregamos la imagen del usuario con sesión iniciada en miniatura en la barra de navegación.

--- src/views/includes/header.php
+++ src/views/includes/header.php
@@ -10,6 +10,16 @@
      }
      # Si existe, tomamos su nombre de usuario
     $username = $_SESSION['user'];
+
+    $sqlGetProfilePhoto = "SELECT fotografia FROM usuarios WHERE usuario = '$username'";
+    $queryGetProfilePhoto = $conn->query($sqlGetProfilePhoto);
+
+    if ($queryGetProfilePhoto->num_rows > 0) {
+        while ($row = $queryGetProfilePhoto->fetch_assoc()) {
+            $foto = $row['fotografia'];
+            $rutaFotoPorDefecto = "../../public/img/profile-default.svg";
+        }
+    }
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -22,7 +32,12 @@
     <h1><a href="dashboard.php">Forum</a></h1>
     <input type="text" placeholder="Buscar">
     <!-- Identificamos al usuario dentro de la interfaz -->
-    <h2 class="identifier" id="identifier"><?php echo $username; ?></h2>
+     <div class="profile">
+        <div class="imgBox">
+            <img src="<?php if($foto != '') { echo $foto; } else { echo $rutaFotoPorDefecto; } ?>" alt="">
+        </div>
+        <h2 class="identifier" id="identifier"><?php echo $username; ?></h2>
+    </div>
     <div class="square"></div>
     <div class="dropdown">
         <a href="profile.php?user=<?php echo $username; ?>">Ver perfil</a>
