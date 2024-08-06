0f1702eaf74c08f9db1b86d35bad27cd62c63d60
Saúl Gutiérrez <62368834+saulgutierrez@users.noreply.github.com>
2024-08-05 19:47:16 -0600

    Modificamos el fichero de datos de login para trabajar con consultas preparadas, para una protección contra posibles inyecciones SQL.

--- src/models/data.php
+++ src/models/data.php
@@ -1,29 +1,39 @@
 <?php
-    require('../../config/connection.php');
+require('../../config/connection.php');
 
-    # Solucion a error: "ya habia iniciado una sesion ignorando session_start()"
-    if (!isset($_SESSION)) {
-        session_start();
-    }
+# Solucion a error: "ya habia iniciado una sesion ignorando session_start()"
+if (!isset($_SESSION)) {
+    session_start();
+}
+
+# Si se recibieron datos desde el frontend, los almacenamos para consulta
+if (isset($_POST['user'], $_POST['password'])) {
+    $user = $_POST['user'];
+    $pass = $_POST['password'];
+
+    $cryptPass = sha1($pass);
     
-    # Si se recibieron datos desde el frontend, los almacenamos para consulta
-    if (isset($_POST['user'], $_POST['password'])) {
-        $user = $_POST['user'];
-        $pass = $_POST['password'];
-
-        $cryptPass = sha1($pass);
-        # Consultamos si los datos coinciden en la base de datos
-        $sql = "SELECT * FROM usuarios WHERE usuario = '$user' AND contrasenia = '$cryptPass'";
-        $result = $conn->query($sql);
-        # Si existe alguna coincidencia, creamos una variable de sesion
-        # para identificar al usuario y mantenerlo dentro del sistema
-        if ($result->num_rows > 0) {
-            if (!isset($_SESSION['user'])) {
-                $_SESSION['user'] = $user;
-                echo 0;
+    $sql = $conn->prepare("SELECT * FROM usuarios WHERE usuario = ? AND contrasenia = ?");
+    
+    if ($sql) {
+        $sql->bind_param("ss", $user, $cryptPass);
+        
+        if ($sql->execute()) {
+            $result = $sql->get_result(); // Obtener el resultado de la consulta
+            if ($result->num_rows > 0) {
+                if (!isset($_SESSION['user'])) {
+                    $_SESSION['user'] = $user;
+                    echo 0;
+                }
+            } else {
+                echo 1;
             }
         } else {
             echo 1;
-        }   
+        }
+        $sql->close(); // Cierra la consulta preparada
+    } else {
+        echo "Error en la preparación de la consulta.";
     }
+}
 ?>
