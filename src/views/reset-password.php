7e033025f454fa9b49ff3c1b90ee4de52b30a3ee
Saúl Gutiérrez <62368834+saulgutierrez@users.noreply.github.com>
2024-09-16 18:54:35 -0600

    Mejoramos el diseño del formulario de recuperación de contraseña, Aún no finalizado.

--- src/views/reset-password.php
+++ src/views/reset-password.php
@@ -11,9 +11,12 @@
 <body>
     <?php include "../views/includes/navbar-forum.php"; ?>
     <form id="recoveryForm">
-        <input type="text" id="userOrEmail" placeholder="Nombre de usuario o correo">
-        <input type="text" id="recoveryCode" placeholder="Codigo de recuperacion">
-        <input type="password" id="newPassword" placeholder="Nueva contraseña">
+        <label for="userOrEmail">Usuario o Email</label>
+        <input type="text" id="userOrEmail">
+        <label for="recoveryCode">Código de recuperación</label>
+        <input type="text" id="recoveryCode">
+        <label for="newPassword">Nueva Contraseña</label>
+        <input type="password" id="newPassword">
         <div class="group-buttons">
             <button type="submit">Confirmar</button>
             <a href="login.php">Atrás</a>
