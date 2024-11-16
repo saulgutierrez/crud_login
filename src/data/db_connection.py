d557dc6193e93d0c40983993fcd7404bc3b2df65
Saúl Gutiérrez <62368834+saulgutierrez@users.noreply.github.com>
2024-11-16 12:42:54 -0600

    Recogemos los posteos de la base de datos con Python, para posteriormente realizar una limpieza de datos.

--- src/data/db_connection.py
+++ src/data/db_connection.py
@@ -20,3 +20,30 @@ def close_connection(connection):
     if connection.is_connected():
         connection.close()
         print("Conexion cerrada correctamente")
+
+# Funcion para obtener los datos de los posteos (titulos y contenidos)
+def fetch_posts_data():
+    connection = create_connection()
+    if connection is None:
+        return []
+
+    query = "SELECT titulo_post, contenido_post FROM post"
+    cursor = connection.cursor()
+    posts_data = []
+
+    try:
+        cursor.execute(query)
+        result = cursor.fetchall()
+        for row in result:
+            post = {
+                'titulo_post':    row[0],
+                'contenido_post':   row[1]
+            }
+            posts_data.append(post)
+    except Error as e:
+        print(f"Error al obtener los datos de los posteos: {e}")
+    finally:
+        cursor.close()
+        close_connection(connection)
+
+        return posts_data
