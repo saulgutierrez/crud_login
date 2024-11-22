c87cd288dc51bc80bace1e7913230a0be22befdd
Saúl Gutiérrez <62368834+saulgutierrez@users.noreply.github.com>
2024-11-21 23:26:32 -0600

    Estilizamos la pantalla FAQ de la aplicación.

--- src/views/faq.php
+++ src/views/faq.php
@@ -3,9 +3,12 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="../../public/svg/faq-icon-black.svg" rel="icon" media="(prefers-color-scheme: light)">
+    <link href="../../public/svg/faq-icon-white.svg" rel="icon" media="(prefers-color-scheme: dark)">
     <title>Frequently Asked Questions</title>
 </head>
 <body>
-    
+    <?php include "includes/navbar-faq.php"; ?>
+
 </body>
 </html>
---
+++ src/views/includes/navbar-faq.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../../public/css/navbar.css">
+    <link rel="stylesheet" href="../public/css/navbar.css">
+    <title>Games DB</title>
+</head>
+<body>
+    <nav>
+        <a href="../../public/index.php" class="logo">
+            <div class="imgBox">
+                <img src="../../public/svg/gamepad.svg" alt="">
+            </div>
+            <div>Games DB</div>
+        </a>
+        <ul class="navbar">
+            <li class="explore hidden">
+                <a href="#">
+                    <div class="imgBox">
+                        <img src="../../public/svg/explore.svg" alt="">
+                    </div>
+                    <div>Explorar</div>
+                </a>
+            </li>
+            <li class="hidden">
+                <a href="faq.php">
+                    <div class="imgBox">
+                        <img src="../../public/svg/faq.svg" alt="">
+                    </div>
+                    <div>FAQ</div>
+                </a>
+            </li>
+            <li class="forum hidden">
+                <a href="../views/login.php">
+                    <div class="imgBox">
+                        <img src="../../public/svg/forum.svg" alt="">
+                    </div>
+                    <div>Foro</div>
+                </a>
+            </li>
+            <li class="menu"><img src="../../public/svg/menu.svg" class="menu-icon" alt=""></li>
+            <div class="square-menu hidden"></div>
+            <div class="menu-opciones hidden"></div>
+        </ul>
+    </nav>
+    <script src="../../src/ui/navbar.js"></script>
+</body>
+</html>
