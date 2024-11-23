8ea8a22a4eeb44a24890d0af0dd11d3112f0087f
Saúl Gutiérrez <62368834+saulgutierrez@users.noreply.github.com>
2024-11-22 23:53:31 -0600

    Maquetación básica para la página FAQ de la aplicación. Aún no finalizado.

--- src/views/faq.php
+++ src/views/faq.php
@@ -5,10 +5,18 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="../../public/svg/faq-icon-black.svg" rel="icon" media="(prefers-color-scheme: light)">
     <link href="../../public/svg/faq-icon-white.svg" rel="icon" media="(prefers-color-scheme: dark)">
+    <link rel="stylesheet" href="../../public/css/faq.css">
     <title>Frequently Asked Questions</title>
 </head>
 <body>
     <?php include "includes/navbar-faq.php"; ?>
-
+    <main class="main-container">
+        <aside id="category-menu">
+            <div>Hola</div>
+        </aside>
+        <div id="registros" class="registros">
+            <div>Hola</div>
+        </div>
+    </main>
 </body>
 </html>
