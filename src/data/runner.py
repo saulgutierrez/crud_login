94bce040f89df084d2c6a083deddafc53a125f2a
Saúl Gutiérrez <62368834+saulgutierrez@users.noreply.github.com>
2024-12-26 18:25:23 -0600

    Incluimos la libreria argparse de Python para incluir el id del usuario como argumento en el comando Python. Aún no finalizado.

--- src/data/runner.py
+++ src/data/runner.py
@@ -1,4 +1,5 @@
 import json
+import argparse
 from dataset import generate_dataset
 from knn_classifier_users import knn_recommendations
 
@@ -7,7 +8,13 @@ def save_results(results):
         json.dump(results, f)
 
 if __name__ == "__main__":
+    # Parsear argumentos del script
+    parser = argparse.ArgumentParser(description="KNN Recommendation Script")
+    parser.add_argument("user_id", type=int, help="ID del usuario logueado")
+    args = parser.parse_args()
+    # Recuperar el id del usuario logueado
+    user_id = args.user_id
+    # Generar el dataset y obtener recomendaciones
     dataset = generate_dataset()
-    user_id = 1
     recommendations = knn_recommendations(user_id, dataset)
     save_results(recommendations)
