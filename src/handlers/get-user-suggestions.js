ab7c0e4854399af7b2d8c16c755cee2d110cdc96
Saúl Gutiérrez <62368834+saulgutierrez@users.noreply.github.com>
2024-12-30 22:25:57 -0600

    Recuperamos el id del usuario con sesión iniciada para enviarlo como parametro en el comando de ejecución del algoritmo KNN con Python. Falta mostrar los datos en el frontend.

--- src/handlers/get-user-suggestions.js
+++ src/handlers/get-user-suggestions.js
@@ -1,12 +1,16 @@
 const peopleSuggestionsBtn = document.querySelector('#show-suggestions');
 
 peopleSuggestionsBtn.addEventListener("click", () => {
-    fetch("../models/run-knn-user-suggestions.php")
-        .then((response) => response.json())
-        .then((data) => {
-
-        })
-        .catch((error) => {
-            console.error("Error al obtener las recomendaciones:", error);
-        });
+    fetch("../models/run-knn-user-suggestions.php", {
+        method: "POST",
+        headers: { "Content-Type": "application/x-www-form-urlencoded" },
+        body: `user_id=${authUserId}`,
+    })
+    .then((response) => response.json())
+    .then((data) => {
+        
+    })
+    .catch((error) => {
+        console.error("Error al obtener las recomendaciones:", error);
+    });
 });
--- src/models/run-knn-user-suggestions.php
+++ src/models/run-knn-user-suggestions.php
@@ -1,23 +1,17 @@
 <?php
-    require('../../config/connection.php');
-
-    if (isset($_SESSION)) {
-        session_start();
+    if ($_SERVER["REQUEST_METHOD"] === "POST") {
+        $user_id = $_POST['user_id'];
+        // Ejecutar el script de Python
+        $command = escapeshellcmd("python3 ../data/runner.py $user_id");
+        $output = shell_exec($command);
+        $json_file = '..data/results_users.json';
+        if (file_exists($json_file)) {
+            // Leer el JSON generado
+            $json = file_get_contents('../data/results_users.json');
+            echo $json;
+        } else {
+            echo json_encode(["error" => "No se pudieron generar las recomendaciones"]);
+        }
+        exit;
     }
-
-    // Obtener el id del usuario con sesion iniciada
-    $user = $_SESSION['user'];
-    $sqlGetIdUser = "SELECT id FROM usuarios WHERE usuario = ?";
-    $stmt = $conn->prepare($sqlGetIdUser);
-    $stmt->bind_param("s", $user);
-    $stmt->execute();
-    $result = $stmt->get_result();
-    $id_fetch = $result->fetch_assoc();
-    $user_id = $id_fetch['id'];
-    // Construir el comando para ejecutar el script de Python, enviando como parámetro el id del usuario logueado
-    $command = escapeshellcmd("python3 ../data/knn_classifier_users.py " . escapeshellarg($user_id));
-    $output = shell_exec($command);
-    $jsonData = file_get_contents('results-suggestions.json');
-    $results = json_decode($jsonData, true);
-    echo json_encode($results);
 ?>
